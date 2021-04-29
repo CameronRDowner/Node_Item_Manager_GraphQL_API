b0b0bf89d53fc229d16cc4095d088a5d3b617c70
Cameron Downer <camerondowner@gmail.com>
2021-04-28 20:54:23 -0500

    initial project files

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+app.listen(3000, () => console.log('server started'));
